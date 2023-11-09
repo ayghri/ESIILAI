0505741262e2f696df131546ee1875c400a9cec0
Ayoub Ghriss <dev@aghriss.net>
2023-11-09 10:07:26 -0700

    no pyplot

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 conda install -n base conda-libmamba-solver
-conda install -c anaconda --solver=libmamba scikit-learn keras tensorflow scikit-learn matplotlib.pyplot pandas
+conda install -c anaconda --solver=libmamba scikit-learn keras tensorflow scikit-learn matplotlib pandas
