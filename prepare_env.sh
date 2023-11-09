df3ddb69d7931b8328a089a2dc1fb35f7ee7144a
Ayoub Ghriss <dev@aghriss.net>
2023-11-09 09:55:59 -0700

    added packages

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,4 +2,4 @@
 conda create -n esiilai
 conda activate esiilai
 conda install -n esiilai conda-libmamba-solve
-conda install -c anaconda --solver=libmamba scikit-image keras tensorflow scikit-learn matplotlib.pyplot
+conda install -c anaconda --solver=libmamba scikit-learn keras tensorflow scikit-learn matplotlib.pyplot pandas
